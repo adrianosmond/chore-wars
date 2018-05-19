5e27e8365dd92cb3c81ced994977eea722e9dafc
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-05-19 18:19:28 +0200

    Rewrote session tests

--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -47,7 +47,6 @@ export function signOut() {
   };
 }
 
-
 export function createGame(userId, playerName) {
   return (dispatch) => {
     const joinCode = createJoinCode();
@@ -61,42 +60,44 @@ export function createGame(userId, playerName) {
       },
     };
 
-    database.ref('games').push().then((ref) => {
+    return database.ref('games').push().then((ref) => {
       const gameKey = ref.key;
       const userData = {
         gameId: gameKey,
       };
       database.ref(`joinCodes/${joinCode}`).set(gameKey);
-      database.ref(`users/${userId}`).set(userData).then(() => {
+      return database.ref(`users/${userId}`).set(userData).then(() => {
         dispatch(setGame(userData));
-        ref.set(gameData);
+        return ref.set(gameData);
       });
     });
   };
 }
 
-export function joinGame(userId, gameToJoin, playerName) {
-  return (dispatch) => {
-    database.ref(`joinCodes/${gameToJoin}`).once('value', (result) => {
-      const gameKey = result.val();
-      const userData = {
-        gameId: gameKey,
-      };
-      database.ref(`users/${userId}`).set(userData).then(() => {
-        database.ref(`games/${gameKey}/players/`).once('value', (users) => {
-          users.forEach(user => user.child('joinCode').ref.set(null));
-        });
-        Promise.all([
-          database.ref(`games/${gameKey}/points/${userId}`),
-          database.ref(`games/${gameKey}/players/${userId}`),
-        ]).then((refs) => {
-          refs[0].set(DEFAULT_POINTS_DATA);
-          refs[1].set(generatePlayerData(playerName));
-          database.ref(`games/${gameKey}/gameIncomplete`).set(null);
-          database.ref(`joinCodes/${gameToJoin}`).set(null);
-          dispatch(setGame(userData));
-        });
-      });
+export function joinGame(userId, joinCode, playerName) {
+  let userData = null;
+  const getGameFromJoinCode = () => new Promise((resolve) => {
+    database.ref(`joinCodes/${joinCode}`).once('value', (result) => {
+      resolve(result.val());
     });
-  };
+  });
+
+  const setUserData = gameKey => new Promise((resolve) => {
+    userData = { gameId: gameKey };
+    database.ref(`users/${userId}`).set(userData).then(() => {
+      resolve(gameKey);
+    });
+  });
+
+  const addUserAndCleanUp = gameKey => Promise.all([
+    database.ref(`games/${gameKey}/points/${userId}`).set(DEFAULT_POINTS_DATA),
+    database.ref(`games/${gameKey}/players/${userId}`).set(generatePlayerData(playerName)),
+    database.ref(`games/${gameKey}/gameIncomplete`).set(null),
+    database.ref(`joinCodes/${joinCode}`).set(null),
+  ]);
+
+  return dispatch => getGameFromJoinCode()
+    .then(setUserData)
+    .then(addUserAndCleanUp)
+    .then(() => dispatch(setGame(userData)));
 }
--- src/actions/sessionActions.test.js
+++ src/actions/sessionActions.test.js
@@ -1,11 +1,14 @@
-import { ActionTypes } from 'constants/constants';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { auth } from 'lib/firebase';
+import { database, auth } from 'lib/firebase';
+import * as utils from 'constants/utils';
+import { ActionTypes, DEFAULT_POINTS_DATA } from 'constants/constants';
+import { DefaultAvatar } from 'constants/avatars';
 import * as sessionActions from './sessionActions';
 
 const authUser = 'fake-auth-user';
 const game = 'fake-game';
+const joinCode = 'joinCode';
 const mockStore = configureMockStore([thunk]);
 
 describe('Session Actions', () => {
@@ -73,59 +76,117 @@ describe('Session Actions', () => {
 
 
   it('can dispatch createGame', () => {
-  // expect(sessionActions.createGame(userId)).toEqual({
-  // return (dispatch) => {
-  //   const joinCode = createJoinCode();
-  //   const gameData = {
-  //     gameIncomplete: true,
-  //     players: {
-  //       [userId]: generatePlayerData(playerName, joinCode),
-  //     },
-  //     points: {
-  //       [userId]: DEFAULT_POINTS_DATA,
-  //     },
-  //   };
+    jest.spyOn(utils, 'createJoinCode').mockImplementation(() => joinCode);
+    const store = mockStore();
+    const userId = 'player1';
+    const playerName = 'Player 1';
 
-  //   database.ref('games').push().then((ref) => {
-  //     const gameKey = ref.key;
-  //     const userData = {
-  //       gameId: gameKey,
-  //     };
-  //     database.ref(`joinCodes/${joinCode}`).set(gameKey);
-  //     database.ref(`users/${userId}`).set(userData).then(() => {
-  //       dispatch(setGame(userData));
-  //       ref.set(gameData);
-  //     });
-  //   });
-  // };
-  // });
+    database.ref().set({
+      games: {},
+      joinCodes: {},
+      users: {},
+    });
+
+    return store.dispatch(sessionActions.createGame(userId, playerName)).then(() => {
+      const data = database.ref().getData();
+      expect(data).toHaveProperty('games');
+      expect(Object.keys(data.games)).toHaveLength(1);
+      const gameKey = Object.keys(data.games)[0];
+      const gameData = data.games[gameKey];
+      expect(gameData).toHaveProperty('gameIncomplete');
+      expect(gameData.gameIncomplete).toBe(true);
+
+      expect(gameData).toHaveProperty('players');
+      expect(gameData.players).toHaveProperty(userId);
+      const playerData = gameData.players[userId];
+      expect(playerData).toEqual(utils.generatePlayerData(playerName, joinCode));
+
+      expect(gameData).toHaveProperty('points');
+      expect(gameData.points).toHaveProperty(userId);
+      const pointsData = gameData.points[userId];
+      expect(pointsData).toEqual(DEFAULT_POINTS_DATA);
+
+      expect(data).toHaveProperty('joinCodes');
+      expect(data.joinCodes).toEqual({
+        [joinCode]: gameKey,
+      });
+
+      expect(data).toHaveProperty('users');
+      expect(data.users).toHaveProperty(userId);
+      expect(data.users).toEqual({
+        [userId]: {
+          gameId: gameKey,
+        },
+      });
+
+      expect(store.getActions()).toEqual([
+        { type: ActionTypes.setGame, game: { gameId: gameKey } },
+      ]);
+    });
   });
 
   it('can dispatch joinGame', () => {
-  // expect(sessionActions.joinGame(userId)).toEqual({
-  // return (dispatch) => {
-  //   database.ref(`joinCodes/${gameToJoin}`).once('value', (result) => {
-  //     const gameKey = result.val();
-  //     const userData = {
-  //       gameId: gameKey,
-  //     };
-  //     database.ref(`users/${userId}`).set(userData).then(() => {
-  //       database.ref(`games/${gameKey}/players/`).once('value', (users) => {
-  //         users.forEach(user => user.child('joinCode').ref.set(null));
-  //       });
-  //       Promise.all([
-  //         database.ref(`games/${gameKey}/points/${userId}`),
-  //         database.ref(`games/${gameKey}/players/${userId}`),
-  //       ]).then((refs) => {
-  //         refs[0].set(DEFAULT_POINTS_DATA);
-  //         refs[1].set(generatePlayerData(playerName));
-  //         database.ref(`games/${gameKey}/gameIncomplete`).set(null);
-  //         database.ref(`joinCodes/${gameToJoin}`).set(null);
-  //         dispatch(setGame(userData));
-  //       });
-  //     });
-  //   });
-  // };
-  // });
+    const store = mockStore();
+    const userId = 'player2';
+    const playerName = 'Player 2';
+    const player1 = {
+      gameId: game,
+    };
+
+    database.ref().set({
+      games: {
+        [game]: {
+          gameIncomplete: true,
+          players: {
+            player1: utils.generatePlayerData('Player 1', joinCode),
+          },
+          points: {
+            player1: DEFAULT_POINTS_DATA,
+          },
+        },
+      },
+      joinCodes: {
+        [joinCode]: game,
+      },
+      users: {
+        player1,
+      },
+    });
+
+    return store.dispatch(sessionActions.joinGame(userId, joinCode, playerName)).then(() => {
+      expect(store.getActions()).toEqual([
+        { type: ActionTypes.setGame, game: { gameId: game } },
+      ]);
+
+      const data = database.ref().getData();
+      expect(data).toHaveProperty('games');
+      expect(Object.keys(data.games).length).toBe(1);
+      const gameKey = Object.keys(data.games)[0];
+      const gameData = data.games[gameKey];
+      expect(gameData).not.toHaveProperty('gameIncomplete');
+
+      expect(gameData).toHaveProperty('players');
+      expect(gameData.players).toHaveProperty(userId);
+      expect(Object.keys(gameData.players)).toHaveLength(2);
+      const playerData = gameData.players[userId];
+      expect(playerData).toEqual(utils.generatePlayerData(playerName));
+
+      expect(gameData).toHaveProperty('points');
+      expect(gameData.points).toHaveProperty(userId);
+      const pointsData = gameData.points[userId];
+      expect(pointsData).toEqual(DEFAULT_POINTS_DATA);
+
+      expect(data).not.toHaveProperty('joinCodes');
+
+      expect(data).toHaveProperty('users');
+      expect(Object.keys(data.users)).toHaveLength(2);
+      expect(data.users).toHaveProperty(userId);
+      expect(data.users).toEqual({
+        player1,
+        [userId]: {
+          gameId: gameKey,
+        },
+      });
+    });
   });
 });
