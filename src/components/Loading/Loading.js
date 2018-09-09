1fa4afc76f8ec1440808afe3edd5f6e1b6429133
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-09-09 17:36:13 +0200

    Can't scroll loading screen

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -7,13 +7,11 @@ import loading2 from './loading2.png';
 
 const Loading = () => (
   <div className="loading">
-    <div className="loading__logo">
-      <div className="loading__image-wrapper">
-        <img src={loading1} alt="" className="loading__image loading__image--1" />
-        <img src={loading2} alt="" className="loading__image loading__image--2" />
-      </div>
-      <h1 className="loading__text">Loading</h1>
+    <div className="loading__image-wrapper">
+      <img src={loading1} alt="" className="loading__image loading__image--1" />
+      <img src={loading2} alt="" className="loading__image loading__image--2" />
     </div>
+    <h1 className="loading__text">Loading</h1>
   </div>
 );
 
