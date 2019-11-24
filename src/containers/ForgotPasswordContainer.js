f8a3dbe84a170c7b7b5cb08bab58200e5e4b99d4
Adrian Osmond <a.c.osmond@gmail.com>
2019-11-24 13:17:01 +0100

    Success and error messages for forgot password

--- src/containers/ForgotPasswordContainer.js
+++ src/containers/ForgotPasswordContainer.js
@@ -1,35 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from 'database';
 import useInput from 'hooks/useInput';
+import useToggle from 'hooks/useToggle';
 import Input from 'components/Input';
 import Button from 'components/Button';
 import Card from 'components/Card';
 import Spacer from 'components/Spacer';
+import Notification from 'components/Notification';
 import FormButtonHolder from 'components/FormButtonHolder';
 
 const LoginContainer = () => {
   const [email, updateEmail] = useInput('');
+
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [error, setError] = useState('');
+
+  const [
+    successMessageVisible,
+    showSuccessMessage,
+    hideSuccessMessage,
+  ] = useToggle(false);
+  const [errorMessageVisible, showErrorMessage, hideErrorMessage] = useToggle(
+    false,
+  );
+
   const onSubmit = e => {
     e.preventDefault();
-    auth.sendPasswordResetEmail(email).catch(err => console.error(err));
+    setIsUpdating(true);
+    auth
+      .sendPasswordResetEmail(email)
+      .then(() => showSuccessMessage())
+      .catch(err => {
+        setError(err.message);
+        showErrorMessage();
+      })
+      .then(() => setIsUpdating(false));
   };
 
+  const isFormInvalid = email.length < 6 || !email.includes('@');
+
   return (
-    <Card title="Forgot your password">
-      <Spacer as="form" onSubmit={onSubmit}>
-        <Input
-          type="email"
-          placeholder="your.name@email.com"
-          label="What is your email address?"
-          value={email}
-          spacing="xs"
-          onChange={updateEmail}
-        />
-        <FormButtonHolder>
-          <Button type="submit">Send password reset</Button>
-        </FormButtonHolder>
-      </Spacer>
-    </Card>
+    <>
+      {successMessageVisible && (
+        <Notification closeNotification={hideSuccessMessage}>
+          A password reset has been sent to {email}
+        </Notification>
+      )}
+      {errorMessageVisible && (
+        <Notification
+          closeNotification={hideErrorMessage}
+          appearance="error"
+          hideAfter={5000}
+        >
+          {error}
+        </Notification>
+      )}
+      <Card title="Forgot your password">
+        <Spacer as="form" onSubmit={onSubmit}>
+          <Input
+            type="email"
+            placeholder="your.name@email.com"
+            label="What is your email address?"
+            value={email}
+            spacing="xs"
+            onChange={updateEmail}
+          />
+          <FormButtonHolder>
+            <Button
+              type="submit"
+              disabled={isFormInvalid || isUpdating}
+              isBusy={isUpdating}
+            >
+              Send password reset
+            </Button>
+          </FormButtonHolder>
+        </Spacer>
+      </Card>
+    </>
   );
 };
 
