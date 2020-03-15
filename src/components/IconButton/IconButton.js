5ca4b7727f90b3c8404fc5855a939ebb25392af3
Adrian Osmond <a.c.osmond@gmail.com>
2020-03-15 08:24:23 +0100

    Allow icon button classes to be extended

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import classnames from 'classnames';
 import classes from './IconButton.module.css';
 
-const IconButton = ({ Icon, ...props }) => (
-  <button className={classes.button} {...props}>
+const IconButton = ({ Icon, className, ...props }) => (
+  <button
+    className={classnames({
+      [classes.button]: true,
+      [className]: true,
+    })}
+    {...props}
+  >
     <Icon className={classes.icon} />
   </button>
 );
